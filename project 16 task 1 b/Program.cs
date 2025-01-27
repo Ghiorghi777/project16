ccb9948212499766033291a398ecfbf0e72e1b40
Ghiorghi777 <goncargeorgij9@gmail.com>
2025-01-27 15:04:27 +0200

    Add files via upload

---
+++ project 16 task 1 b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_16_task_1_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 41; i <= 80; i += 10)
+            {
+                for (int j = i; j <= i + 9; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
